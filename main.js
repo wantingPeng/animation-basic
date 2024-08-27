fa382279a5a907ec5266b8ff6115e2d39f4025f1
wanting <wantingpeng.cici@gmail>
2024-08-27 20:42:35 +0200

    finish responsive

---
+++ main.js
@@ -0,0 +1,11 @@
+const hamberger=document.querySelector('#hamburger')
+const nav=document.querySelector('#nav')
+hamberger.addEventListener('click',function(){
+  opacity.classList.add('tigger')
+})
+
+const opacity=document.querySelector('.opacity')
+const close=document.querySelector('.close')
+close.addEventListener('click',function(){
+  opacity.classList.remove('tigger')
+})
